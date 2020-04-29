817ff8c3d8495de08d21d10991f84f54c9a13358
Tamer Saadeh <tamersaadeh@gmail.com>
2020-04-29 12:35:00 +0200

    Initial commit

---
+++ build-custom-kernel.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+command -v realpath >/dev/null 2>&1 || function realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+SOURCEDIR="$(dirname $(realpath $0))"
+WORKDIR="$(mktemp -d)"
+
+pushd "$WORKDIR" > /dev/null
+
+# download Photon sources:
+echo -n "Downloading sources ($WORKDIR)..."
+curl -L https://github.com/vmware/photon/archive/master.tar.gz -o photon.tar.gz -s > /dev/null
+echo "done"
+
+# extract sources:
+echo -n "Extracting sources..."
+tar xf photon.tar.gz
+mv photon-master photon
+echo "done"
+
+# Patch kernel
+echo -n "Patching kernel SPEC..."
+pushd photon > /dev/null
+find "$SOURCEDIR/patches/linux-spec" -type f -name *.patch -exec patch --no-backup-if-mismatch -p0 --fuzz=0 \< {} \;
+popd > /dev/null
+echo "done"
+
+# Patch build script
+echo -n "Patching build script..."
+pushd photon > /dev/null
+find "$SOURCEDIR/patches/build-scrip" -type f -name *.patch -exec patch --no-backup-if-mismatch -p0 --fuzz=0 \< {} \;
+popd > /dev/null
+echo "done"
+
+# Build RPM
+photon/tools/scripts/build_spec.sh photon/SPECS/linux/linux-esx.spec
+
+# Move output to sources
+echo -n "Copying RPMs..."
+mv photon/SPECS/linux/stage "$SOURCEDIR"
+echo "done"
+
+popd > /dev/null
+
+# Clean up tmp files
+echo -n "Cleaning up temporary files..."
+rm -rf $WORKDIR
+echo "done"
